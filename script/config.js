f92ea60cce8d7c67a1063a58064926a4aae8efa9
iZhih <i.zhih@mail.ru>
2025-07-11 16:14:06 +0500

    Added Game class and its functions in main.js: 	- loadSpriteSheet(); 	- render();
    Created Config class in config.js:
    	This class contains canvas size, velocity, pipes interval and sprites coords
    
    Added Canvas class and its functions in canvas.js:
    	- createCanvas();
    	- getContext();
    	- drawImage();

--- script/config.js
+++ script/config.js
@@ -1,118 +1,125 @@
 export default class Config {
-  static width = 276;
-  static height = 490;
-  static velocityX = this.width; // начальная скорость полёта птицы в пикселях/сек
-  static pipesInterval = (54 * 4) / this.velocityX; // интервал между трубами, привязанный к скорости
+  constructor() {
+    this.width = 276;
+    this.height = 490;
+    this.velocityX = this.width; // начальная скорость полёта птицы в пикселях/сек
+    this.pipesInterval = (54 * 4) / this.velocityX; // интервал между трубами, привязанный к скорости
 
-  // координаты спрайтов в спрайтшите
-  static sprite = {
-    background: {
-      srcX: 0,
-      srcY: 0,
-      width: 276,
-      height: 228,
-    },
-
-    foreground: {
-      srcX: 276,
-      srcY: 0,
-      width: 224,
-      height: 112,
-    },
-
-    bird: {
-      width: 34,
-      height: 26,
-
-      sprite0: {
-        srcX: 276,
-        srcY: 112,
+    // координаты спрайтов в спрайтшите
+    this.sprite = {
+      src: './img/sprite-sheet.png',
+      sky: {
+        color: 'skyblue',
       },
 
-      sprite1: {
-        srcX: 276,
-        srcY: 138,
-      },
-
-      sprite2: {
-        srcX: 276,
-        srcY: 164,
-      },
-    },
-
-    medal: {
-      width: 44,
-      height: 44,
-      none: {
-        srcX: 312,
-        srcY: 112,
-      },
-      bronze: {
-        srcX: 360,
-        srcY: 158,
-      },
-      silver: {
-        srcX: 360,
-        srcY: 112,
-      },
-      gold: {
-        srcX: 312,
-        srcY: 158,
-      },
-    },
-
-    pipe: {
-      width: 52,
-      height: 400,
-
-      lower: {
-        srcX: 502,
-        srcY: 0,
-      },
-
-      upper: {
-        srcX: 554,
-        srcY: 0,
-      },
-    },
-
-    label: {
-      getReady: {
+      background: {
         srcX: 0,
-        srcY: 228,
+        srcY: 0,
+        width: 276,
+        height: 228,
+      },
+
+      foreground: {
+        srcX: 276,
+        srcY: 0,
+        width: 224,
+        height: 112,
+      },
+
+      bird: {
+        width: 34,
+        height: 26,
+
+        sprite0: {
+          srcX: 276,
+          srcY: 112,
+        },
+
+        sprite1: {
+          srcX: 276,
+          srcY: 138,
+        },
+
+        sprite2: {
+          srcX: 276,
+          srcY: 164,
+        },
+      },
+
+      medal: {
+        width: 44,
+        height: 44,
+        none: {
+          srcX: 312,
+          srcY: 112,
+        },
+        bronze: {
+          srcX: 360,
+          srcY: 158,
+        },
+        silver: {
+          srcX: 360,
+          srcY: 112,
+        },
+        gold: {
+          srcX: 312,
+          srcY: 158,
+        },
+      },
+
+      pipe: {
+        width: 52,
+        height: 400,
+
+        lower: {
+          srcX: 502,
+          srcY: 0,
+        },
+
+        upper: {
+          srcX: 554,
+          srcY: 0,
+        },
+      },
+
+      label: {
+        getReady: {
+          srcX: 0,
+          srcY: 228,
+          width: 174,
+          height: 44,
+        },
+
+        gameOver: {
+          srcX: 193,
+          srcY: 228,
+          width: 188,
+          height: 44,
+        },
+      },
+
+      guide: {
+        srcX: 0,
+        srcY: 227,
         width: 174,
-        height: 44,
+        height: 116,
       },
 
-      gameOver: {
-        srcX: 193,
-        srcY: 228,
-        width: 188,
-        height: 44,
+      scoreWindow: {
+        srcX: 174,
+        srcY: 227,
+        width: 226,
+        height: 116,
       },
-    },
 
-    guide: {
-      srcX: 0,
-      srcY: 227,
-      width: 174,
-      height: 116,
-    },
-
-    scoreWindow: {
-      srcX: 174,
-      srcY: 227,
-      width: 226,
-      height: 116,
-    },
-
-    button: {
-      start: {
-        srcX: 246,
-        srcY: 400,
-        width: 82,
-        height: 28,
+      button: {
+        start: {
+          srcX: 246,
+          srcY: 400,
+          width: 82,
+          height: 28,
+        },
       },
-    },
-  };
+    };
+  }
 }
--- script/main.js
+++ script/main.js
@@ -1,5 +1,32 @@
-import Game from './game.js';
+import Config from './config.js';
+import Canvas from './canvas.js';
 
-window.onload = function () {
-  const game = new Game('field');
-};
+class Game {
+  constructor(aNodeId) {
+    this.config = new Config();
+    this.canvas = new Canvas(aNodeId, this.config.width, this.config.height);
+    this.spriteSheet = this.loadSpriteSheet(this.config.sprite.src);
+    this.spriteSheet.onload = this.render(this);
+
+  }
+
+  loadSpriteSheet(src) {
+    const result = new Image();
+    result.src = src;
+
+    return result;
+  }
+
+  render() {
+    console.log('Запущена функция render()');
+    console.log(this);
+    const sprite = this.config.sprite.foreground;
+    sprite.destX = 100;
+    sprite.destY = 100;
+    this.canvas.drawImage(this.spriteSheet, sprite);
+
+    // window.requestAnimationFrame(this.render.bind(this));
+  }
+}
+
+const game = new Game('field');

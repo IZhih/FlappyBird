fafbf734df7e14ffd0fe08e32c732d85116ceb88
iZhih <i.zhih@mail.ru>
2025-07-16 12:36:09 +0500

    drawImage() debug data was removed from render() to constructor() in Game class in main.js
    Added requestAnimationFrame() in render function in Game class in main.js

--- script/main.js
+++ script/main.js
@@ -6,8 +6,20 @@ class Game {
     this.config = new Config();
     this.canvas = new Canvas(aNodeId, this.config.width, this.config.height);
     this.spriteSheet = this.loadSpriteSheet(this.config.sprite.src);
+
+    // для отладки функции drawImage()
+    const sprite = this.config.sprite.foreground;
+    sprite.destX = 0;
+    sprite.destY = this.config.height - sprite.height;
+    sprite.destW = this.config.width;
+    sprite.destH = sprite.height;
+    sprite.srcW = sprite.width;
+    sprite.srcH = sprite.height;
+
+    this.screen = [];
+    this.screen.push(sprite);
     this.spriteSheet.onload = () => {
-      this.render();
+      this.render(this.screen[0]);
     };
   }
 
@@ -19,18 +31,9 @@ class Game {
   }
 
   render(scene) {
-    console.log('Запущена функция render()');
-    console.log(this);
-    const sprite = this.config.sprite.foreground;
-    sprite.destX = 0;
-    sprite.destY = this.config.height - sprite.height;
-    sprite.destW = this.config.width;
-    sprite.destH = sprite.height;
-    sprite.srcW = sprite.width;
-    sprite.srcH = sprite.height;
-    this.canvas.drawImage(this.spriteSheet, sprite);
+    this.canvas.drawImage(this.spriteSheet, scene);
 
-    // window.requestAnimationFrame(this.render.bind(this, scene));
+    window.requestAnimationFrame(this.render.bind(this, scene));
   }
 }
 
